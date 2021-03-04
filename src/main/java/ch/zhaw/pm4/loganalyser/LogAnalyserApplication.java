b270abe6ddd675089c744de6d7cda92e5168a937
meletlea <meletlea@students.zhaw.ch>
2021-03-04 16:12:03 +0100

    refactor package to lowercase characters

--- src/main/java/ch/zhaw/pm4/loganalyser/LogAnalyserApplication.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/LogAnalyserApplication.java
@@ -1,4 +1,4 @@
-package ch.zhaw.pm4.LogAnalyser;
+package ch.zhaw.pm4.loganalyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
