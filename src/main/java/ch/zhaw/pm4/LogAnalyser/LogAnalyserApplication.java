83863dbd5f9b500b1dbca9039897847d0034e7a4
Carl Lubojanski <lubojcar@students.zhaw.ch>
2021-03-04 15:40:40 +0100

    added a quick hello method

--- src/main/java/ch/zhaw/pm4/LogAnalyser/LogAnalyserApplication.java
+++ src/main/java/ch/zhaw/pm4/LogAnalyser/LogAnalyserApplication.java
@@ -2,12 +2,27 @@ package ch.zhaw.pm4.LogAnalyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class LogAnalyserApplication {
+@RestController
+public class LogAnalyserApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LogAnalyserApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogAnalyserApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hallo Welt!";
 	}
 
 }
