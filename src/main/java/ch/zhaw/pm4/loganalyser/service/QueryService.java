f2b1b3eb6cf5997cb25aa2235263a1dcf9a12758
Carl Lubojanski <lubojcar@students.zhaw.ch>
2021-03-09 15:49:20 +0100

    Replaced creation of sample data with dto's

--- src/main/java/ch/zhaw/pm4/loganalyser/service/QueryService.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/service/QueryService.java
@@ -1,19 +1,40 @@
 package ch.zhaw.pm4.loganalyser.service;
 
+import ch.zhaw.pm4.loganalyser.model.dto.ColumnDTO;
+import ch.zhaw.pm4.loganalyser.model.dto.HeaderDTO;
+import ch.zhaw.pm4.loganalyser.model.dto.TableDTO;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class QueryService {
 
-     public List<String[]> getSampleLogsByQuery() {
-         List<String[]> log = new ArrayList<>();
-         log.add(new String[] {"Date", "Type", "Message"});
-         log.add(new String[] {"01.03.2021", "info", "Service started"});
-         log.add(new String[] {"02.03.2021", "warning", "Could not interpret xyz"});
-         log.add(new String[] {"03.03.2021", "error", "FileNotFound test.txt"});
-         return log;
+    private final Logger logger = Logger.getLogger(QueryService.class.getName());
+
+     public TableDTO getSampleLogsByQuery() {
+
+         List<ColumnDTO> tableData = new ArrayList<>();
+         List<HeaderDTO> headers = new ArrayList<>();
+         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+         try {
+            tableData.add(new ColumnDTO(sdf.parse("01.03.2021"), "info", "Service started"));
+            tableData.add(new ColumnDTO(sdf.parse("02.03.2021"), "warning", "Could not interpret xyz"));
+            tableData.add(new ColumnDTO(sdf.parse("03.03.2021"), "error", "FileNotFound test.txt"));
+         } catch (ParseException e) {
+             logger.log(Level.WARNING, "Unable to parse the given date", e);
+         }
+
+         headers.add(new HeaderDTO("Date", "date"));
+         headers.add(new HeaderDTO("Type", "type"));
+         headers.add(new HeaderDTO("Message", "Message"));
+
+        return new TableDTO(headers, tableData);
+
      }
 }
