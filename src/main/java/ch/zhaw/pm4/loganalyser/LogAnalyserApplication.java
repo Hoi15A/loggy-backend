8a83c837545caade0ee0f455093d87339e0385a2
Carl Lubojanski <lubojcar@students.zhaw.ch>
2021-03-09 14:49:05 +0100

    Disabled cors
    Added some code to temporarily disable cors.
    
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>

--- src/main/java/ch/zhaw/pm4/loganalyser/LogAnalyserApplication.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/LogAnalyserApplication.java
@@ -3,7 +3,14 @@ package ch.zhaw.pm4.loganalyser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Collections;
 
 @SpringBootApplication
 public class LogAnalyserApplication extends SpringBootServletInitializer {
@@ -16,5 +23,23 @@ public class LogAnalyserApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(LogAnalyserApplication.class, args);
 	}
+
+	/**
+     * Filter Cors
+     * @return FilterRegistrationBean<CorsFilter>
+     */
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 
 }
