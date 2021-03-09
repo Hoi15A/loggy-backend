a10fc32b0008d4649f9cef3a6008db53e4b5e90f
Carl Lubojanski <lubojcar@students.zhaw.ch>
2021-03-09 11:30:26 +0100

    Added query rest controller
    Added a query rest controller to provide test data for the frontend.
    
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>
    Co-Authored-By: Stefan Holzer <holzest3@students.zhaw.ch>
    Co-Authored-By: Austin Albrecht <2957+albreaus@users.noreply.github.zhaw.ch>

---
+++ src/main/java/ch/zhaw/pm4/loganalyser/controller/QueryController.java
@@ -0,0 +1,24 @@
+package ch.zhaw.pm4.loganalyser.controller;
+
+import ch.zhaw.pm4.loganalyser.service.QueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/query")
+public class QueryController {
+
+    private final QueryService queryService;
+
+    @GetMapping("sample")
+    public ResponseEntity<List<String[]>> getSampleQuery() {
+        return ResponseEntity.ok(queryService.getSampleLogsByQuery());
+    }
+
+}
