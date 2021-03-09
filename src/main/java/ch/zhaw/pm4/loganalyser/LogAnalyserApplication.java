2de0d6f6e60ba9fc5abb2319375861c1a5b31542
Carl Lubojanski <lubojcar@students.zhaw.ch>
2021-03-09 11:29:27 +0100

    Removed test mapping
    Removed request mappin, that was only used for a simple test
    
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>
    Co-Authored-By: Stefan Holzer <holzest3@students.zhaw.ch>
    Co-Authored-By: Austin Albrecht <2957+albreaus@users.noreply.github.zhaw.ch>

--- src/main/java/ch/zhaw/pm4/loganalyser/LogAnalyserApplication.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/LogAnalyserApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class LogAnalyserApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -18,11 +15,6 @@ public class LogAnalyserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogAnalyserApplication.class, args);
-	}
-
-	@RequestMapping("/")
-	public String hello() {
-		return "Hallo Welt!";
 	}
 
 }
