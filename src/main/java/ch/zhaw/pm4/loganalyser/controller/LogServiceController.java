1ef371ec111e474b8dbf9395cd8af21046199267
Carl Lubojanski <lubojcar@students.zhaw.ch>
2021-03-21 00:18:09 +0100

    implemented LogServiceController createService method

---
+++ src/main/java/ch/zhaw/pm4/loganalyser/controller/LogServiceController.java
@@ -0,0 +1,25 @@
+package ch.zhaw.pm4.loganalyser.controller;
+
+import ch.zhaw.pm4.loganalyser.model.dto.LogServiceDTO;
+import ch.zhaw.pm4.loganalyser.service.LogServiceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/service")
+public class LogServiceController {
+
+    private final LogServiceService logServiceService;
+
+    @PostMapping("/")
+    public ResponseEntity<String> createService(@RequestBody final LogServiceDTO logServiceDTO) {
+        logServiceService.createLogService(logServiceDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
