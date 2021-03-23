ac5f9f15b3b59c823d72500b4344977f773bf901
Carl Lubojanski <lubojcar@students.zhaw.ch>
2021-03-23 10:35:29 +0100

    added validation annotations

--- src/main/java/ch/zhaw/pm4/loganalyser/model/dto/LogConfigDTO.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/model/dto/LogConfigDTO.java
@@ -4,11 +4,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 @NoArgsConstructor
 @Getter @Setter
 public class LogConfigDTO {
+    @NotBlank
     private String name;
+    @Min(0)
     private int columnCount;
+    @Min(0)
     private int headerLength;
+    @NotEmpty
     private String separator;
 }
--- src/main/java/ch/zhaw/pm4/loganalyser/model/dto/LogServiceDTO.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/model/dto/LogServiceDTO.java
@@ -1,17 +1,25 @@
 package ch.zhaw.pm4.loganalyser.model.dto;
 
+import ch.zhaw.pm4.loganalyser.model.log.LogServiceLocation;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.net.URI;
 
 @NoArgsConstructor
 @Getter @Setter
 public class LogServiceDTO {
+    @NotBlank
     private String logDirectory;
+    @NotBlank
     private String name;
     private String description;
     private URI image;
+    @NotNull
+    private LogServiceLocation location;
+    @NotNull
     private LogConfigDTO logConfigDTO;
 }
