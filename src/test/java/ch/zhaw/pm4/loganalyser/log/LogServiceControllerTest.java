ba5317ff9daf4551f1e381b68a5ae6a4757e7695
holzest3 <holzerstefan@hotmail.com>
2021-03-23 14:26:35 +0100

    improve test for getAllLogServices

--- src/test/java/ch/zhaw/pm4/loganalyser/log/LogServiceControllerTest.java
+++ src/test/java/ch/zhaw/pm4/loganalyser/log/LogServiceControllerTest.java
@@ -1,7 +1,9 @@
 package ch.zhaw.pm4.loganalyser.log;
 
 import ch.zhaw.pm4.loganalyser.controller.LogServiceController;
+import ch.zhaw.pm4.loganalyser.model.dto.LogServiceDTO;
 import ch.zhaw.pm4.loganalyser.service.LogServiceService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +13,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -92,14 +97,30 @@ class LogServiceControllerTest {
     @Test
     void getAllLogServices() {
 
+        Set<LogServiceDTO> data = new HashSet<>();
+        LogServiceDTO logServiceDTO1 = new LogServiceDTO();
+        LogServiceDTO logServiceDTO2 = new LogServiceDTO();
+        logServiceDTO1.setName("Test1");
+        logServiceDTO2.setName("Test2");
+        data.add(logServiceDTO1);
+        data.add(logServiceDTO2);
+
+        Mockito.when(logServiceService.getAllLogServices()).thenReturn(data);
+
         try {
             mockMvc.perform(MockMvcRequestBuilders
                     .get("/service/all")
                     .accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andExpect(MockMvcResultMatchers.jsonPath("$").exists());
+                    .andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.[*].name").isNotEmpty())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.[*].name",
+                            Matchers.anyOf(Matchers.hasItem(logServiceDTO1.getName()),
+                                    Matchers.hasItem(logServiceDTO2.getName()))))
+                    .andDo(MockMvcResultHandlers.print());
         } catch (Exception e) {
             fail(e);
         }
+        Mockito.verify(logServiceService, Mockito.times(1)).getAllLogServices();
     }
 }
