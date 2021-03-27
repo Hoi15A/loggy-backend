c67e9c72f5d8bb5a4d7a99503c7d7432c2cf5b2c
Carl Lubojanski <lubojcar@students.zhaw.ch>
2021-03-27 16:46:35 +0100

    added enablewebmvc annotation

--- src/main/java/ch/zhaw/pm4/loganalyser/config/CORSAdvice.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/config/CORSAdvice.java
@@ -2,9 +2,11 @@ package ch.zhaw.pm4.loganalyser.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CORSAdvice implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
