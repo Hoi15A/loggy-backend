0487e3a819c920aa5e3329733388d9a38019bad3
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-03-30 12:20:24 +0200

    added logconfigservice to provide database access

---
+++ src/main/java/ch/zhaw/pm4/loganalyser/service/LogConfigService.java
@@ -0,0 +1,35 @@
+package ch.zhaw.pm4.loganalyser.service;
+
+import ch.zhaw.pm4.loganalyser.exception.RecordNotFoundException;
+import ch.zhaw.pm4.loganalyser.model.dto.LogConfigDTO;
+import ch.zhaw.pm4.loganalyser.model.log.LogConfig;
+import ch.zhaw.pm4.loganalyser.repository.LogConfigRepository;
+import ch.zhaw.pm4.loganalyser.util.DTOMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class LogConfigService {
+
+    private final LogConfigRepository logConfigRepository;
+
+    public List<LogConfigDTO> getAllLogConfigs() {
+        return logConfigRepository
+                .findAll()
+                .stream()
+                .map(DTOMapper::mapLogConfigToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public LogConfigDTO getLogConfigById(String id) {
+        Optional<LogConfig> logConfigOptional = logConfigRepository.findById(id);
+        if (logConfigOptional.isEmpty()) throw new RecordNotFoundException(id);
+        return DTOMapper.mapLogConfigToDTO(logConfigOptional.get());
+    }
+
+}
