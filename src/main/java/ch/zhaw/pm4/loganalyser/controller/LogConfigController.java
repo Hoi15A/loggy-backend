8137a6083c837ee480971f7d6753a9118be3c5b2
holzest3 <holzerstefan@hotmail.com>
2021-04-05 14:36:12 +0200

    add endpoint in controller to save log configs

--- src/main/java/ch/zhaw/pm4/loganalyser/controller/LogConfigController.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/controller/LogConfigController.java
@@ -3,12 +3,16 @@ package ch.zhaw.pm4.loganalyser.controller;
 import ch.zhaw.pm4.loganalyser.model.dto.LogConfigDTO;
 import ch.zhaw.pm4.loganalyser.service.LogConfigService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @AllArgsConstructor
@@ -17,6 +21,17 @@ import java.util.List;
 public class LogConfigController {
 
     private final LogConfigService logConfigService;
+
+    /**
+     * Creates a log service and saves into the database based on the provided {@link LogConfigDTO}.
+     * @param logConfigDTO Data transfer object containing the data that should be saved in the database.
+     * @return {@link ResponseEntity} with status 201 and an empty body.
+     */
+    @PostMapping("/")
+    public ResponseEntity<String> createService(@Valid @RequestBody final LogConfigDTO logConfigDTO) {
+        logConfigService.createLogConfig(logConfigDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
     /**
      * Returns a transformed list of all log configs inside the database.
