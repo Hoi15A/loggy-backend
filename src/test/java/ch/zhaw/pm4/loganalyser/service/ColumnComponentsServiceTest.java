d9c1f84d6a38832bb21a3d316b12146e98812542
pedernin <pedernin@students.zhaw.ch>
2021-04-08 22:41:57 +0200

    added ColumnComponentsServiceTest #11

---
+++ src/test/java/ch/zhaw/pm4/loganalyser/service/ColumnComponentsServiceTest.java
@@ -0,0 +1,71 @@
+package ch.zhaw.pm4.loganalyser.service;
+
+import ch.zhaw.pm4.loganalyser.exception.RecordNotFoundException;
+import ch.zhaw.pm4.loganalyser.model.dto.ColumnComponentDTO;
+import ch.zhaw.pm4.loganalyser.model.log.column.ColumnType;
+import ch.zhaw.pm4.loganalyser.repository.ColumnComponentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+@DataJpaTest
+class ColumnComponentsServiceTest {
+
+    @Autowired
+    ColumnComponentRepository columnComponentRepository;
+    ColumnComponentService columnComponentService;
+
+    @BeforeEach
+    void setUp() {
+        columnComponentService = new ColumnComponentService(columnComponentRepository);
+    }
+
+    @Test
+    @Sql("classpath:sql/getcolumncomponents.sql")
+    void testGetAllColumnComponents() {
+        List<ColumnComponentDTO> columnDTOS = columnComponentService.getAllColumnComponents();
+        Assertions.assertEquals(1, columnDTOS.get(0).getId());
+        Assertions.assertEquals(ColumnType.DATE, columnDTOS.get(0).getColumnType());
+        Assertions.assertEquals("ff", columnDTOS.get(0).getFormat());
+        Assertions.assertEquals("Host", columnDTOS.get(0).getName());
+
+        Assertions.assertEquals(2, columnDTOS.get(1).getId());
+        Assertions.assertEquals(ColumnType.DATE, columnDTOS.get(1).getColumnType());
+        Assertions.assertEquals("ff", columnDTOS.get(1).getFormat());
+        Assertions.assertEquals("User", columnDTOS.get(1).getName());
+    }
+
+    @Test
+    @Sql("classpath:sql/getcolumncomponents.sql")
+    void testGetAllColumnComponentsById() {
+        ColumnComponentDTO columnDTO = columnComponentService.getColumnComponentById(1L);
+        Assertions.assertEquals(1, columnDTO.getId());
+        Assertions.assertEquals(ColumnType.DATE, columnDTO.getColumnType());
+        Assertions.assertEquals("ff", columnDTO.getFormat());
+        Assertions.assertEquals("Host", columnDTO.getName());
+    }
+
+    @Test
+    void testGetConfigNotAvailable() {
+        Assertions.assertThrows(RecordNotFoundException.class, () -> columnComponentService.getColumnComponentById(5L));
+    }
+
+    @Test
+    void testCreateColumnComponent() {
+        ColumnComponentDTO testColumn = new ColumnComponentDTO();
+        testColumn.setName("Host1");
+        testColumn.setFormat("-");
+        testColumn.setId(3L);
+        testColumn.setColumnType(ColumnType.MESSAGE);
+
+        columnComponentService.createColumn(testColumn);
+
+        Assertions.assertEquals(1, columnComponentRepository.count());
+
+    }
+}
---
+++ src/test/resources/sql/getcolumncomponents.sql
@@ -0,0 +1,2 @@
+insert into components (id, column_type, format, name) values (1, 0, 'ff','Host');
+insert into components (id, column_type, format, name) values (2, 0, 'ff','User');
