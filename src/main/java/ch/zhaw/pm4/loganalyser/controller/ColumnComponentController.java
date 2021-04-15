814b9bade6877d0255825e09d85aa70b1cb1f661
holzest3 <holzerstefan@hotmail.com>
2021-04-15 22:40:35 +0200

    very minor refactors for naming and added some javadocs at the same time #13

--- src/main/java/ch/zhaw/pm4/loganalyser/controller/ColumnComponentController.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/controller/ColumnComponentController.java
@@ -1,12 +1,18 @@
 package ch.zhaw.pm4.loganalyser.controller;
 
 import ch.zhaw.pm4.loganalyser.model.dto.ColumnComponentDTO;
-import ch.zhaw.pm4.loganalyser.model.dto.LogConfigDTO;
 import ch.zhaw.pm4.loganalyser.service.ColumnComponentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -29,13 +35,22 @@ public class ColumnComponentController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-
+    /**
+     * Updates a column component and saves into the database based on the provided {@link ColumnComponentDTO}.
+     * @param columnComponentDTO Data transfer object containing the data that should be saved in the database.
+     * @return {@link ResponseEntity} with status 200 and the updated {@link ColumnComponentDTO}.
+     */
     @PutMapping("/{id}")
-    public ResponseEntity<ColumnComponentDTO> updateColumn(@Valid @RequestBody final ColumnComponentDTO componentDTO) {
-        columnComponentService.updateColumn(componentDTO);
-        return ResponseEntity.ok(componentDTO);
+    public ResponseEntity<ColumnComponentDTO> updateColumn(@Valid @RequestBody final ColumnComponentDTO columnComponentDTO) {
+        columnComponentService.updateColumn(columnComponentDTO);
+        return ResponseEntity.ok(columnComponentDTO);
     }
 
+    /**
+     * Deletes a column component and saves into the database based on the provided {@link ColumnComponentDTO}.
+     * @param id of {@link ColumnComponentDTO}
+     * @return {@link ResponseEntity} with status 200 and the updated {@link ColumnComponentDTO}.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<ColumnComponentDTO> deleteColumnComponentById(@PathVariable long id) {
         return ResponseEntity.ok(columnComponentService.deleteColumnComponentById(id));
@@ -52,7 +67,7 @@ public class ColumnComponentController {
 
     /**
      * Returns a transformed column component with the matching id.
-     * @param id that should be fetched from the database.
+     * @param id of {@link ResponseEntity} that should be fetched from the database.
      * @return {@link ResponseEntity} with status 200 and a {@link ColumnComponentDTO} inside the body.
      */
     @GetMapping("/{id}")
