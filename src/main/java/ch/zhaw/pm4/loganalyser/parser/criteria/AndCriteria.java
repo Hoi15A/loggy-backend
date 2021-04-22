b6e15779864dae9e414fb7abd86e703ac36433c9
holzest3 <holzerstefan@hotmail.com>
2021-04-22 10:47:50 +0200

    implement interface AndCriteria

--- src/main/java/ch/zhaw/pm4/loganalyser/parser/criteria/AndCriteria.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/parser/criteria/AndCriteria.java
@@ -1,4 +1,21 @@
 package ch.zhaw.pm4.loganalyser.parser.criteria;
 
+import java.util.List;
+
 public class AndCriteria implements Criteria {
+
+    private final Criteria criteria;
+    private final Criteria otherCriteria;
+
+    public AndCriteria(Criteria criteria, Criteria otherCriteria) {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<String[]> meetCriteria(List<String[]> rows) {
+
+        List<String[]> firstCriteriaPersons = criteria.meetCriteria(rows);
+        return otherCriteria.meetCriteria(firstCriteriaPersons);
+    }
 }
--- src/main/java/ch/zhaw/pm4/loganalyser/parser/criteria/Criteria.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/parser/criteria/Criteria.java
@@ -1,4 +1,7 @@
 package ch.zhaw.pm4.loganalyser.parser.criteria;
 
+import java.util.List;
+
 public interface Criteria {
+    public List<String[]> meetCriteria(List<String[]> rows);
 }
