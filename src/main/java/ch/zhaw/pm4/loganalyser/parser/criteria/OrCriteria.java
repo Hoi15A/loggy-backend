bb284ccd0622acf0817d58b624b90c9a85c47273
holzest3 <holzerstefan@hotmail.com>
2021-04-23 18:09:37 +0200

    implement OrCriteria

--- src/main/java/ch/zhaw/pm4/loganalyser/parser/criteria/OrCriteria.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/parser/criteria/OrCriteria.java
@@ -1,10 +1,25 @@
 package ch.zhaw.pm4.loganalyser.parser.criteria;
 
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
+@AllArgsConstructor
 public class OrCriteria implements Criteria {
+    
+    private final Criteria criteria;
+    private final Criteria otherCriteria;
+
     @Override
     public List<String[]> meetCriteria(List<String[]> rows, int key) {
-        return null;
+        List<String[]> firstCriteriaItems = criteria.meetCriteria(rows, key);
+        List<String[]> otherCriteriaItems = otherCriteria.meetCriteria(rows, key);
+
+        for (String[] row: otherCriteriaItems) {
+            if(!firstCriteriaItems.contains(row)){
+                firstCriteriaItems.add(row);
+            }
+        }
+        return firstCriteriaItems;
     }
 }
