b37005d26e4f3d69bbe2beb69cd6a317e69ce33e
meletlea <meletlea@students.zhaw.ch>
2021-04-27 11:28:00 +0200

    remove unused imports

--- src/main/java/ch/zhaw/pm4/loganalyser/LogAnalyserApplication.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/LogAnalyserApplication.java
@@ -3,14 +3,7 @@ package ch.zhaw.pm4.loganalyser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import java.util.Collections;
 
 @SpringBootApplication
 public class LogAnalyserApplication extends SpringBootServletInitializer {
