bc60c5934b706a1776f7186310974f93cf15e4cc
meletlea <meletlea@students.zhaw.ch>
2021-05-04 12:45:22 +0200

    #17 add range criteria test class with empty test cases

---
+++ src/test/java/ch/zhaw/pm4/loganalyser/test/query/parser/criteria/RangeCriteriaTest.java
@@ -0,0 +1,178 @@
+package ch.zhaw.pm4.loganalyser.test.query.parser.criteria;
+
+import org.junit.jupiter.api.Test;
+
+class RangeCriteriaTest {
+
+
+    /* ****************************************************************************************************************
+     * POSITIVE TESTS
+     * ****************************************************************************************************************/
+
+    @Test
+    void testRangeCriteria() {
+
+    }
+
+    /* ****************************************************************************************************************
+     * NEGATIVE TESTS
+     * ****************************************************************************************************************/
+
+    @Test
+    void testRangeCriteria_NullListArgument() {
+
+    }
+
+    @Test
+    void testRangeCriteria_ColumnTypeNotSet() {
+
+    }
+
+    @Test
+    void testRangeCriteria_ColumnTypeInvalid() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidColumnIndex_IndexOutOfBound_Negative() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidColumnIndex_IndexOutOfBound_Positive() {
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Number
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Test
+    void testRangeCriteria_InvalidNumber_Null() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidNumberFromValue_Null() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidNumberToValue_Null() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidNumber() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidNumberFromValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidNumberToValue() {
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // IP
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_Null() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_Null_FromValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_Null_ToValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpPattern() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpPattern_FromValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpPattern_ToValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_MaxValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_MaxValue_FromValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_MaxValue_ToValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_NonIpCharacters() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_NonIpCharacters_FromValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidIpValues_NonIpCharacters_ToValue() {
+
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // IP
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Test
+    void testRangeCriteria_InvalidDateValue_Null() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidDateValue_Null_FromValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidDateValue_Null_ToValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidDateValue_Format() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidDateValue_Format_FromValue() {
+
+    }
+
+    @Test
+    void testRangeCriteria_InvalidDateValue_Format_ToValue() {
+
+    }
+
+}
