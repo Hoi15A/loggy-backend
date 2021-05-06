fef2632287aac6d98a9f217a178dd85d37517f38
meletlea <meletlea@students.zhaw.ch>
2021-05-06 09:22:04 +0200

    #17 add RegexCriteriaTest

--- src/main/java/ch/zhaw/pm4/loganalyser/query/criteria/RegexCriteria.java
+++ src/main/java/ch/zhaw/pm4/loganalyser/query/criteria/RegexCriteria.java
@@ -10,10 +10,14 @@ import java.util.stream.Collectors;
  */
 @RequiredArgsConstructor
 public class RegexCriteria implements Criteria {
+
     private final String regex;
 
     @Override
     public List<String[]> apply(List<String[]> rows, int columnIndex) {
-        return rows.stream().filter(row -> row[columnIndex].matches(regex)).collect(Collectors.toList());
+        return rows.stream()
+                .filter(row -> row[columnIndex].matches(regex))
+                .collect(Collectors.toList());
     }
+
 }
